7ce55ff2222530c3260371f7534b1fa1be57af05
integgy <matthewregidor@gmail.com>
2023-12-01 08:54:30 -0800

    create frontend store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react';
+import configureStore from './store/store';
 import './index.css';
+
+const store = configureStore()
+
+if (import.meta.env.MODE !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ frontend/src/store/store.js
@@ -0,0 +1,23 @@
+import {legacy_createStore, applyMiddleware, compose, combineReducers} from "redux"
+import thunk from "redux-thunk"
+
+
+const rootReducer = combineReducers({
+
+});
+
+let enhancer;
+if (import.meta.env.MODE === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState = {}) => {
+    return legacy_createStore(rootReducer, preloadedState, enhancer)
+}
+
+export default configureStore
