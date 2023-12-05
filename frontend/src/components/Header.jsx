56b805283b4cb5fd05ba81b831370b97e43f62ae
integgy <matthewregidor@gmail.com>
2023-12-05 10:54:27 -0800

    remove console log

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ import "./Header.css"
 
  
 
- const Header = ({path}) => {
+ const Header = () => {
     const dispatch = useDispatch()
 
     const currentUser = useSelector(state => {
@@ -15,42 +15,36 @@ import "./Header.css"
         return state.users[id]
     })
 
-    const pageNotFound = (path) => {
-        console.log(path)
-    }
-    
+
 
     return (
-        <>
-            <nav className="header">
-                <Link className="home" to={"/"}>
-                    <img className="logo"src="logo/logo.png" alt="logo" />
-                </Link>
-                <div className="header-options">
-                    {currentUser ? (
-                            <ul className="user">
-                                <li className="user-icon"><FontAwesomeIcon icon={faUser}/></li>                 
-                                <li>Hello,<br/> {currentUser.name}
-                                    <div className="user-menu">
-                                        <Link onClick={()=> dispatch(logoutUser(currentUser.id))}>Logout</Link>
-                                    </div>
-                                </li>
-                            </ul> 
-                        
-                    ): (
-                        <ul className="new-menu">              
-                            <li>
-                                <Link to={"login"}>Login</Link>
-                                <div>
-                                    <p>{"Don't have an account?"} <Link to={"signup"}>Start here</Link></p>
+        <nav className="header">
+            <Link className="home" to={"/"}>
+                <img className="logo"src="logo/logo.png" alt="logo" />
+            </Link>
+            <div className="header-options">
+                {currentUser ? (
+                        <ul className="user">
+                            <li className="user-icon"><FontAwesomeIcon icon={faUser}/></li>                 
+                            <li>Hello,<br/> {currentUser.name}
+                                <div className="user-menu">
+                                    <Link onClick={()=> dispatch(logoutUser(currentUser.id))}>Logout</Link>
                                 </div>
                             </li>
                         </ul> 
-                    )}
-                </div>
-            </nav>
-            {pageNotFound(path)}
-        </>
+                    
+                ): (
+                    <ul className="new-menu">              
+                        <li>
+                            <Link to={"login"}>Login</Link>
+                            <div>
+                                <p>{"Don't have an account?"} <Link to={"signup"}>Start here</Link></p>
+                            </div>
+                        </li>
+                    </ul> 
+                )}
+            </div>
+        </nav>
     )
  }
  export default Header
