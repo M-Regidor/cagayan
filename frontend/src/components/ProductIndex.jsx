991d61c8ffd5445501a77fd2d53f7c515d552d77
integgy <matthewregidor@gmail.com>
2023-12-08 11:22:27 -0800

    refactor tea index

--- frontend/src/components/ProductIndex.jsx
+++ frontend/src/components/ProductIndex.jsx
@@ -1,8 +1,8 @@
 import { useDispatch, useSelector } from "react-redux"
-import { Link } from "react-router-dom"
 import { fetchProducts, selectProductsArray } from "../store/productReducer"
 import { useEffect } from "react"
 import "./ProductIndex.css"
+import ProductIndexItem from "./ProductIndexItem"
 
 
 
@@ -16,22 +16,12 @@ const ProductIndex = () => {
     return (
         <div className="product-background">
             <ul className="product-container">
-                {products.map(product => 
-                    <li className="product-info-container" key={product.id}>
-                        <div className="product-info-top">                            
-                            <Link to={`products/${product.id}`} > 
-                                <div className="product-img"><img src={product.imgUrls} alt="" /></div>
-                            </Link>
-                        </div>
-                        <div className="product-info-bottom">
-                            <div className="product-name"><Link to={`products/${product.id}`}>{product.name}</Link></div>
-                            <div className="product-category">{product.category}</div>
-                            <div className="product-rating">Rating: {product.rating}</div> 
-                            <div className="product-price">${product.price}</div>
-                            <div className="product-delivery">estimated delivery date: Someday or never</div>
-                        </div>
-                    </li>
-                )}
+                {products.map(product => (
+                    <ProductIndexItem
+                        key={product.id}
+                        product={product}
+                    />
+                ))}
             </ul>
         </div>
     )
--- frontend/src/components/ProductIndexItem.jsx
+++ frontend/src/components/ProductIndexItem.jsx
@@ -1,7 +1,22 @@
-const ProductIndexItem = () => {
+import { Link } from "react-router-dom"
+
+const ProductIndexItem = ({product}) => {
 
     return (
-        
+        <li className="product-info-container" key={product.id}>
+            <div className="product-info-top">                            
+                <Link to={`products/${product.id}`} > 
+                    <div className="product-img"><img src={product.imgUrls} alt="" /></div>
+                </Link>
+            </div>
+            <div className="product-info-bottom">
+                <div className="product-name"><Link to={`products/${product.id}`}>{product.name}</Link></div>
+                <div className="product-category">{product.category}</div>
+                <div className="product-rating">Rating: {product.rating}</div> 
+                <div className="product-price">${product.price}</div>
+                <div className="product-delivery">estimated delivery date: Someday or never</div>
+            </div>
+        </li>
     )
 }
 
