953b33c3c04049c6432082d88adcfc3af428f196
integgy <matthewregidor@gmail.com>
2024-01-17 01:46:19 -0800

    fix side scroll and product index render

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="link-icons">
+                <Link to={"https://integgy.github.io/matt-portfolio"}>Matthew Regidor</Link>
                 <Link to={"https://github.com/integgy"}><img src="/assets/icons8-github-256.png" alt="" /></Link>
                 <Link to={"https://www.linkedin.com/in/matthew-regidor-74a5a9259/"}><img src="/assets/icons8-linkedin-150.png" alt="" /></Link>
             </div>
--- frontend/src/components/ProductComponents/ProductIndex.jsx
+++ frontend/src/components/ProductComponents/ProductIndex.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { fetchProducts, filterProducts, selectProductsArray } from "../../store/productReducer"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "./ProductIndex.css"
 import ProductIndexItem from "./ProductIndexItem"
 
@@ -10,6 +10,7 @@ import ProductIndexItem from "./ProductIndexItem"
 const ProductIndex = ({category, keyword}) => {
     const dispatch = useDispatch()
     let products = useSelector(selectProductsArray)
+    const [loading, setLoading] = useState(null)
     
     const shuffleProducts = (arr) => {
         return arr.sort(()=> Math.random() - 0.5);
@@ -28,30 +29,37 @@ const ProductIndex = ({category, keyword}) => {
     }
 
     useEffect(()=> {
+        setLoading(true)
+
         if (category){
-            dispatch(fetchProducts(category))
+            dispatch(fetchProducts(category)).finally(() => setLoading(false))
         } else if (keyword){
-            dispatch(filterProducts(keyword))
+            dispatch(filterProducts(keyword)).finally(() => setLoading(false))
         } else {
-            dispatch(fetchProducts())
+            dispatch(fetchProducts()).finally(() => setLoading(false))
         }
     }, [dispatch, category,keyword])
 
-    return (
-        <div className="product-background">
-            <div className="product-index-container">
-                <h2>{title}</h2>
-                {products.length === 0 ? <h3>Sorry nothing found for {keyword}</h3> : null}
-                <ul className="product-container">
-                    {products.map(product => (
-                        <ProductIndexItem
-                            key={product.id}
-                            product={product}
-                        />
-                    ))}
-                </ul>
+
+        return (
+            <div className="product-background">
+                {!loading ? 
+                <div className="product-index-container">
+                    <h2>{title}</h2>
+                    {products.length === 0 ? <h3>Sorry nothing found for {keyword}</h3> : null}
+                    <ul className="product-container">
+                        {products.map(product => (
+                            <ProductIndexItem
+                                key={product.id}
+                                product={product}
+                            />
+                        ))}
+                    </ul>
+                </div>
+                :
+                <h2>loading...</h2>
+                }
             </div>
-        </div>
-    )
+        )
 }
 export default ProductIndex
